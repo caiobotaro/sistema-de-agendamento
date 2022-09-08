c8b917249aee210ed8e8f434592ed33dcd4c1f91
cbotaro <cbotaro@uolinc.com>
2022-09-07 21:18:51 -0300

    Atividade 08 - Criação do layout com componentes e uso do conceito de data-binding

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./body.component.css']
 })
 export class BodyComponent implements OnInit {
+  isHideProperty = false;
 
   constructor() { }
 
